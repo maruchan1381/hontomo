06d16a22b34f07d5dea05d5939808da0e845bc8d
maruchan1381 <haru0328mh@gmail.com>
2021-01-10 15:08:52 +0900

    ユーザーモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :books
+
+  with_options presence: true do
+    validates :nickname
+    validates :email, uniqueness: true
+    validates :password, length: { minimum: 6 },
+                         format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  end
 end
